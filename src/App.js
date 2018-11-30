4aa99f352ab88f5b687b43d6fbada16dcf038f7f
Chia <cdiaz91cd@gmail.com>
2018-11-29 23:23:00 -0300

    home created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
+          <Home />
         </header>
       </div>
     );
