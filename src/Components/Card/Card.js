259759d68e296084a15cdd926963dbaae58af6b1
Chia <cdiaz91cd@gmail.com>
2019-03-18 20:36:58 -0300

    adopt page

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import './card.css';
-// import icon1 from '../img/icon1.png';
-// import icon2 from '../img/icon2.png';
-// import icon3 from '../img/icon3.png';
-// import icon4 from '../img/icon4.png';
+import icon1 from '../img/icon1.png';
+import icon2 from '../img/icon2.png';
+import icon3 from '../img/icon3.png';
+import icon4 from '../img/icon4.png';
 import Information from '../Information/Information';
 
 
 class Card extends Component {
     render() {
         let { information } = this.props;
-        console.log('acaaaa ', information[0].steps)
+        console.log('acaaaa ', information[0])
 
 
         return (
             <div>
                 <div className="main">
-                    {/* <div className="image">
+                    <div className="image">
                         <img src={icon1} alt="mascota">
                         </img>
                         <img src={icon2} alt="mascota">
@@ -24,10 +24,10 @@ class Card extends Component {
                         <img src={icon3} alt="mascota">
                         </img>
                         <img src={icon4} alt="mascota">
-                        </img></div> */}
+                        </img></div>
 
 
-                    <div className="image">
+                    {/* <div className="image">
                         {information.images.map((image, index) => {
                             return <img src={image.imagePath} className="imagePath" key={index}></img>
                         })}
@@ -41,7 +41,7 @@ class Card extends Component {
                         {information.steps.map((dataDetail, index) => {
                             return <div className="row" key={index}>{dataDetail.text}</div>
                         })}
-                    </div>
+                    </div> */}
                 </div>
             </div>
         );
